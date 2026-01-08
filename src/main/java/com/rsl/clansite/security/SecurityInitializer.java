492ce624f0c81b5fb7ca6c6a2f1ce618e1ba7675
Martijn Jansen <martijnjansen91@gmail.com>
2026-01-08 20:38:16 +0100

    delete useless file

--- src/main/java/com/rsl/clansite/security/SecurityInitializer.java
+++
@@ -1,6 +0,0 @@
-package com.rsl.clansite.security;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-}
