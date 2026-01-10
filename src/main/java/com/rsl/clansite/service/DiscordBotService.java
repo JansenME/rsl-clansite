1e56995c7925429d321afd50b2460d4d45a19ded
Martijn Jansen <martijnjansen91@gmail.com>
2026-01-10 14:07:33 +0100

    add first slash command to discord bot

--- src/main/java/com/rsl/clansite/service/DiscordBotService.java
+++ src/main/java/com/rsl/clansite/service/DiscordBotService.java
@@ -5,6 +5,9 @@ import jakarta.annotation.PreDestroy;
 import lombok.extern.slf4j.Slf4j;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -13,7 +16,7 @@ import java.util.EnumSet;
 
 @Slf4j
 @Service
-public class DiscordBotService {
+public class DiscordBotService extends ListenerAdapter {
     @Value("${discord.bot-token}")
     private String token;
 
@@ -23,22 +26,35 @@ public class DiscordBotService {
     public void startBot() {
         try {
             log.info("Starting Discord Bot...");
-            // We need intents to see members and status
             EnumSet<GatewayIntent> intents = EnumSet.of(
                     GatewayIntent.GUILD_MEMBERS,
-                    GatewayIntent.GUILD_MESSAGES,
-                    GatewayIntent.GUILD_VOICE_STATES // if you want voice tracking later
+                    GatewayIntent.GUILD_MESSAGES
             );
 
             jda = JDABuilder.createDefault(token)
                     .enableIntents(intents)
+                    .addEventListeners(this)
                     .build();
 
             jda.awaitReady(); // Wait until connected
             log.info("Discord Bot is ONLINE as: {}", jda.getSelfUser().getAsTag());
 
+            jda.updateCommands().addCommands(
+                    Commands.slash("hello", "I will help with anything you want!")
+            ).queue();
         } catch (Exception e) {
             log.error("Failed to start Discord Bot", e);
+        }
+    }
+
+    @Override
+    public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
+        if (event.getName().equals("hello")) {
+            event.reply("Fuck off!")
+                    .setEphemeral(false)
+                    .flatMap(v ->
+                            event.getHook().editOriginalFormat("Fuck off!")
+                    ).queue();
         }
     }
 
