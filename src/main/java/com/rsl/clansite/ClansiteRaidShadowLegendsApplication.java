6cbb0d2dbeaa093c8ba41c6fb55af3e155c2e456
Martijn Jansen <martijnjansen91@gmail.com>
2026-01-13 15:33:14 +0100

    Add correct timezone to the Database and Last Online

--- src/main/java/com/rsl/clansite/ClansiteRaidShadowLegendsApplication.java
+++ src/main/java/com/rsl/clansite/ClansiteRaidShadowLegendsApplication.java
@@ -1,8 +1,11 @@
 package com.rsl.clansite;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableScheduling
@@ -12,4 +15,8 @@ public class ClansiteRaidShadowLegendsApplication {
 		SpringApplication.run(ClansiteRaidShadowLegendsApplication.class, args);
 	}
 
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
 }
