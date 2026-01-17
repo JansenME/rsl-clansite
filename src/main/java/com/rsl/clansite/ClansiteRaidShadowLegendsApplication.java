a46900d587e0be8df628a00574fc615280d14ba7
Martijn Jansen <martijnjansen91@gmail.com>
2026-01-17 20:48:13 +0100

    stabilize production logging to file -> add check whether it worked

--- src/main/java/com/rsl/clansite/ClansiteRaidShadowLegendsApplication.java
+++ src/main/java/com/rsl/clansite/ClansiteRaidShadowLegendsApplication.java
@@ -1,13 +1,19 @@
 package com.rsl.clansite;
 
 import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.TimeZone;
+import java.util.logging.Logger;
 
+@Slf4j
 @SpringBootApplication
 @EnableScheduling
 public class ClansiteRaidShadowLegendsApplication extends SpringBootServletInitializer {
@@ -19,5 +25,20 @@ public class ClansiteRaidShadowLegendsApplication extends SpringBootServletIniti
 	@PostConstruct
 	public void init() {
 		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
+
+	@EventListener(ApplicationReadyEvent.class)
+	public void logActiveLogLevel() {
+		if (log.isTraceEnabled()) {
+			log.info("🔍 LOGGING CHECK: Application is running with TRACE level enabled.");
+		} else if (log.isDebugEnabled()) {
+			log.info("🔍 LOGGING CHECK: Application is running with DEBUG level enabled.");
+		} else if (log.isInfoEnabled()) {
+			log.info("🔍 LOGGING CHECK: Application is running with INFO level enabled.");
+		} else if (log.isWarnEnabled()) {
+			log.warn("🔍 LOGGING CHECK: Application is running with WARN level enabled. INFO logs will be hidden!");
+		} else {
+			log.error("🔍 LOGGING CHECK: Application is running with ERROR level enabled. Most logs will be hidden!");
+		}
 	}
 }
