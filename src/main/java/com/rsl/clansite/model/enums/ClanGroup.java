56c43a3d6ab83265bc6f06ea5e449ba5a6ba7962
Martijn Jansen <martijnjansen91@gmail.com>
2026-02-09 14:27:32 +0100

    add actual name to the website for display

--- src/main/java/com/rsl/clansite/model/enums/ClanGroup.java
+++ src/main/java/com/rsl/clansite/model/enums/ClanGroup.java
@@ -1,16 +1,17 @@
 package com.rsl.clansite.model.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum ClanGroup {
-    T1("T1"),
-    T2("T2");
+    T1("T1", "Fury of the Fallen"),
+    T2("T2", "Raid Elite Newbz Own");
 
     private final String name;
+    private final String displayName;
 
-    ClanGroup(final String name) {
+    ClanGroup(final String name, final String displayName) {
         this.name = name;
-    }
-
-    public String getName() {
-        return name;
+        this.displayName = displayName;
     }
 }
